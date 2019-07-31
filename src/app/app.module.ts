ab5d018a4ecaed68dad81de457a9fd90c754f31b
Kaveri Moger <kaveri.moger@fundingcircle.com>
2019-07-31 16:38:37 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertComponent } from './product-alert/product-alert.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ShippingComponent } from './shipping/shipping.component'
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductAlertComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    TopBarComponent,
+    ShippingComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {
+      path: '', component: ProductListComponent
+    },
+    {
+      path: 'products/:productId', component: ProductDetailsComponent
+    },
+    {
+      path:'cart', component: CartComponent
+    },
+    {
+      path:'shipping', component: ShippingComponent
+    }
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product.ts
@@ -0,0 +1,17 @@
+export const products = [
+  {
+  name: 'Laptop',
+  price: 500,
+  description: 'work on all computer related works'
+},
+{
+  name: 'Bottle',
+  price: 300,
+  description: 'To drink water'
+},
+{
+  name: 'Books',
+  price: 800,
+  description: 'To get information'
+}
+]
